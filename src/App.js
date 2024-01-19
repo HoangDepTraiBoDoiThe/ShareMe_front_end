44d92bd7f0ded27294a8f1dc8375ea3586d7c083
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-01-19 08:23:23 +0700

    clean

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import Home from './container/Home'
 import Login from './components/login'
-
+import { FetchUserLocalStore } from './utils/FetchUser'
 
 const App = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const user = FetchUserLocalStore()
+    if (user)
+    {
+      navigate('/')
+    }
+    else {
+      navigate('/login')
+    }
+  }, [])
+  
   return (
     <Routes>
       <Route path = "login" element = {<Login />} />
